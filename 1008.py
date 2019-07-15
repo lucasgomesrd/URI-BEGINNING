fedeb63d6976aa46da76a35aae4cc6f757bb5912
lucasgomesrd <44147174+lucasgomesrd@users.noreply.github.com>
2019-07-14 23:58:40 -0300

    Create 1008.py

---
+++ 1008.py
@@ -0,0 +1,8 @@
+numfuncionario = int(input())
+hrstrab = int(input())
+valorhr = float(input())
+
+salario = float((hrstrab) * (valorhr))
+
+print("NUMBER = %d" % numfuncionario)
+print("SALARY = U$ %0.2f" % salario)
