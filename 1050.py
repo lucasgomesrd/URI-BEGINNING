18e28059124b4a34785a4339b52578524303f755
Lucas Gomes <44147174+lucasgomesrd@users.noreply.github.com>
2022-03-21 12:27:52 -0300

    Create 1050.py

---
+++ 1050.py
@@ -0,0 +1,21 @@
+DDD = int(input())
+
+if DDD == 61:
+    print('Brasilia')
+elif DDD == 71:
+    print('Salvador')
+elif DDD == 11:
+    print('Sao Paulo')
+elif DDD == 21:
+    print('Rio de Janeiro')
+elif DDD == 32:
+    print('Juiz de Fora')
+elif DDD == 19:
+    print('Campinas')
+elif DDD == 27:
+    print('Vitoria')
+elif DDD == 31:
+    print('Belo Horizonte')
+elif DDD != 61 and  DDD != 71 and DDD != 11 and DDD != 21 and DDD != 32 and DDD != 19 and DDD != 27 and DDD != 31:
+    print('DDD nao cadastrado')
+    
